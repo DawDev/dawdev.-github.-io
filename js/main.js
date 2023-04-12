44e44083de4db32579cda843b9d4ace77cd8c03e
Deiwyy <kaprolekdawidek@gmail.com>
2023-04-12 09:34:26 +0200

    Changed the title to goals instead of mission, added a main.js with an object with the profile info

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+const profileData = {
+    username: '',
+    email: '',
+    birthDate: '',
+    pfpUrl: '',
+    bio: '',
+    projects: [{}, {}],
+    education: [],
+    skills: [],
+    bannerColor: '',
+    socials: []
+};
