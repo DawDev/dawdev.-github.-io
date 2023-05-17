763aef0163b546bb6ff819c7384823d9165f2896
Deiwyy <kaprolekdawidek@gmail.com>
2023-05-17 11:57:00 +0200

    Changed the profile page to look a bit different, added a random quote generator thingy, now fetching from profiledata/quotes.json removed the unresponsive design and made it responsive

--- js/main.js
+++ js/main.js
@@ -1,33 +1,55 @@
 
+const bannerElem = document.querySelector('.banner');
+const usernameElem = document.querySelector('.username');
+const profileImgElem = document.querySelector('.profile-image');
+const skillListElem = document.querySelector('.skill-list');
+const aboutMe = document.querySelector('.about-me');
+const quoteElement = document.querySelector('.quote');
+const quoteAuthorElement = document.querySelector('.quote-author');
+
+// const profileData = {
+//     username: 'DawDev',
+//     email: 'dawkaprol@gmail.com',
+//     birthDate: '07-08-2005',
+//     pfpUrl: './img/profile_picture.jpg',
+//     bio: 'Lorem Ipusm idrc rn',
+//     projects: [{}, {}],
+//     education: ['VMBO-KADER', 'MBO-4 SOFTWARE DEV'],
+//     skills: ['a', 'b', 'c', 'd'],
+//     bannerColor: 'red',
+//     socials: ['dont-care']
+// };
+
+
+fetch('/profiledata/quotes.json')
+    .then(fetchedData => fetchedData.json())
+    .then(quotes => {
+        const quote = quotes[getRandomInt(quotes.length)];
+        quoteElement.innerText = quote.text;
+        quoteAuthorElement.innerHTML = `- ${quote.author != null ? quote.author : 'Unknown' }`;
+    });
+
+
+fetch('/profiledata/profiledata.json')
+    .then(fetchedData => fetchedData.json())
+    .then(jsonData => addData(jsonData));
 
 
 
-const profileData = {
-    username: 'DawDev',
-    email: 'dawkaprol@gmail.com',
-    birthDate: '07-08-2005',
-    pfpUrl: './img/profile_picture.jpg',
-    bio: 'Lorem Ipusm idrc rn',
-    projects: [{}, {}],
-    education: ['VMBO-KADER', 'MBO-4 SOFTWARE DEV'],
-    skills: ['a', 'b', 'c', 'd'],
-    bannerColor: 'red',
-    socials: ['dont-care']
-};
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const bannerElem = document.querySelector('.banner');
-    const usernameElem = document.querySelector('.username');
-    const profileImgElem = document.querySelector('.profile-image');
-    const skillListElem = document.querySelector('.skill-list');
-
+function addData(profileData) {
     bannerElem.style.backgroundColor = profileData.bannerColor;
     profileImgElem.src = profileData.pfpUrl;
     usernameElem.innerText = profileData.username;
-
+    aboutMe.innerText = profileData.aboutMe;
+    
     profileData.skills.forEach((skill) => {
         skillListElem.innerHTML += `<li class="list-group-item">${skill}</li>`; 
     });
+}
 
-});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
